cfba1932433d55a7e7ab39ff16947468f3d5f833
Adeel Zahid <m.adeel2144@gmail.com>
2020-07-24 20:10:33 +0500

    Fix Error (Final)

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-import React from 'react';
-import Axios from 'axios';
-import DisplayWeather from './components/DisplayWeather';
-import Navbar from './components/Navbar'
-import './App.css';
+import React from "react";
+import Axios from "axios";
+import DisplayWeather from "./components/DisplayWeather";
+import Navbar from "./components/Navbar";
+import "./App.css";
 
 class App extends React.Component {
-
-
   //state
   state = {
     coords: {
-      latitude: 31,   //31.3976598
-      longitude: 73   //73.1320449
+      latitude: 31.3976598,
+      longitude: 73.1320449
     },
-    weather : {},
-    inputData: ""
-  }
+    weather: {},
+    inputData: "",
+  };
 
-componentDidMount() {
-  // get device location 
+  componentDidMount() {
+    // get device location
 
-  if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition( (position) => {
-      let newCoords = {
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude
-      }
-      this.setState({coords:newCoords})
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        let newCoords = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        };
+        this.setState({ coords: newCoords });
 
-      Axios.get(`http://api.weatherstack.com/current?access_key=55b1ea0965849941a2b1fa55a72f9698&query=
-      ${this.state.coords.latitude},${this.state.coords.longitude}`).then((res, rej) => {
-        let useWeather = {
+        Axios.get(
+          `http://api.weatherstack.com/current?access_key=55b1ea0965849941a2b1fa55a72f9698&query=
+      ${this.state.coords.latitude},${this.state.coords.longitude}`
+        ).then((res, rej) => {
+          let useWeather = {
             temperature: res.data.current.temperature,
             description: res.data.current.weather_descriptions[0],
             location: res.data.location.name,
@@ -40,52 +40,56 @@ componentDidMount() {
             pressure: res.data.current.pressure,
             precip: res.data.current.precip,
             humidity: res.data.current.humidity,
-            img: res.data.current.weather_icons
-        }
-        this.setState({weather: useWeather})
-      })
-
-
-    })
+            img: res.data.current.weather_icons,
+          };
+          this.setState({ weather: useWeather });
+        });
+      });
+    } else {
+      console.log("Not supported");
+    }
   }
-    
-  else{
-    console.log("Not supported")
-  }
-}
 
-change= (value) => {
-  this.setState({inputData: value})
-}
+  change = (value) => {
+    this.setState({ inputData: value });
+  };
   changeWeather = (event) => {
     event.preventDefault();
-    console.log(this.state.inputData)
-    Axios.get(`http://api.weatherstack.com/current?access_key=55b1ea0965849941a2b1fa55a72f9698&query=${this.state.inputData}`)
-      .then((res) => {
-        let useWeather = {
-          temperature: res.data.current.temperature,
-          description: res.data.current.weather_descriptions[0],
-          location: res.data.location.name,
-          region: res.data.location.region,
-          country: res.data.location.country,
-          wind_speed: res.data.current.wind_speed,
-          pressure: res.data.current.pressure,
-          precip: res.data.current.precip,
-          humidity: res.data.current.humidity,
-          img: res.data.current.weather_icons
+    console.log(this.state.inputData);
+    Axios.get(
+      `http://api.weatherstack.com/current?access_key=55b1ea0965849941a2b1fa55a72f9698&query=${this.state.inputData}`
+    ).then((res) => {
+      try {
+        if (res) {
+          let useWeather = {
+            temperature: res.data.current.temperature,
+            description: res.data.current.weather_descriptions[0],
+            location: res.data.location.name,
+            region: res.data.location.region,
+            country: res.data.location.country,
+            wind_speed: res.data.current.wind_speed,
+            pressure: res.data.current.pressure,
+            precip: res.data.current.precip,
+            humidity: res.data.current.humidity,
+            img: res.data.current.weather_icons,
+          };
+          this.setState({ weather: useWeather });
         }
-        this.setState({ weather: useWeather })
-      })
+      } catch (error) {
+        console.log(res.data.error.type);
+        alert("City Not Found .. ");
+      }
+    });
+  };
 
-  }
-
-
-
-  render(){
+  render() {
     return (
       <div className="App">
         <div className="container">
-          <Navbar changeWeather={this.changeWeather} changeRegion={this.change}/>
+          <Navbar
+            changeWeather={this.changeWeather}
+            changeRegion={this.change}
+          />
           <DisplayWeather weatherData={this.state.weather} />
         </div>
       </div>
@@ -94,4 +98,3 @@ change= (value) => {
 }
 
 export default App;
- 

9e4498183058baf80fcf1f40ab129f27c8c493c0
Adeel Zahid <m.adeel2144@gmail.com>
2020-07-24 17:58:11 +0500

    weather Dispaly Static is Complete ..

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
+import Axios from 'axios';
+import DisplayWeather from './components/DisplayWeather'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+
+  //state
+  state = {
+    coords: {
+      latitude: 31,   //31.3976598
+      longitude: 73   //73.1320449
+    },
+    weather : {}
+  }
+
+componentDidMount() {
+  // get device location 
+
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition( (position) => {
+      let newCoords = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      }
+      this.setState({coords:newCoords})
+
+      Axios.get(`http://api.weatherstack.com/current?access_key=55b1ea0965849941a2b1fa55a72f9698&query=
+      ${this.state.coords.latitude},${this.state.coords.longitude}`).then((res, rej) => {
+        let useWeather = {
+            temperature: res.data.current.temperature,
+            description: res.data.current.weather_descriptions[0],
+            location: res.data.location.name,
+            region: res.data.location.region,
+            country: res.data.location.country,
+            wind_speed: res.data.current.wind_speed,
+            pressure: res.data.current.pressure,
+            precip: res.data.current.precip,
+            humidity: res.data.current.humidity,
+            img: res.data.current.weather_icons
+        }
+        this.setState({weather: useWeather})
+      })
+
+
+    })
+  }
+    
+  else{
+    console.log("Not supported")
+  }
+}
+
+
+
+  render(){
+    return (
+      <div className="App">
+        <div className="container">
+          <DisplayWeather weatherData={this.state.weather} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+ 
